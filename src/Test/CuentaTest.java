a55ce8585f4e868a0140607e01e0c83053c22d07
Prieto <Prieto@Prieto>
2024-10-08 11:09:09 +0200

    Version 1 - Prueba fallida

---
+++ src/Test/CuentaTest.java
@@ -0,0 +1,40 @@
+package Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CuentaTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testIngresar() {
+		fail("Prueba Fallida");
+	}
+	
+	@Test
+	void testRetirar() {
+		fail("Prueba Fallida");
+	}
+		
+
+}
